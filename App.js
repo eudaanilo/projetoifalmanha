bb4caa8a34112862b6509d0184fd1cccafa249d2
eudaanilo <dsa7@aluno.ifal.edu.br>
2022-09-23 11:58:45 -0300

    Adding StatusBar and Render

--- App.js
+++ App.js
@@ -1,20 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+//Classes
+// import React, {Component} from "react";
+// import {View, Text, StatusBar} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+// class App extends Component{
+//   render(){
+//     return(
+//       <View>
+//         <StatusBar></StatusBar>
+//         <Text>Olá ifal e mundo</Text>
+//       </View>
+//     );
+//   }
+// }
+// function App(){
+//   return(
+//     <View>
+//       <statusbar> </statusbar>
+//       <Text>Olá mundo!</Text>
+//       <Text>Meu primeiro APP!!!</Text>
+//     </View>
+//   );
+// }
+
+// export default App;
+
+//-----------------------------------------------
+
+
+
+//imagens e texto
+
+import React from "react";
+
+import {View, Text, Image} from 'react-native';
+
+export default function App(){
+  let nome = 'professor'
+
+  return(
+    <View>
+      <Text>IFAL 2022</Text>
+      <Text style={{color:'#ff0000', fontSize: 25, margin: 60}}>Danilo da Silva Alves</Text>
+      <Image source={{uri:'https://www2.ifal.edu.br/o-ifal/comunicacao/arquivos/logos/copy_of_IFALvertical.png'}}
+      style={{width:150, height:150}}
+      />
+
     </View>
-  );
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
